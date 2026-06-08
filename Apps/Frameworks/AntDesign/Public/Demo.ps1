b23a2973a22e8dcc91634d3353d1db5845800fc5
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2026-06-08 15:13:55 -0500

    Refactor PowerShell Script.

---
+++ Apps/Frameworks/AntDesign/Public/Demo.ps1
@@ -0,0 +1,30 @@
+function New-AntDesignDemo {
+    [CmdletBinding()]
+    [OutputType([object[]])]
+    param()
+
+    @{
+        type       = 'antd-docs'
+        id         = 'antdesign-docs'
+        title      = 'Ant Design Components'
+        overview   = 'Component documentation for the PowerShell Universal Ant Design framework. The examples shown in the page are generated from the module command help so the docs and comment-based help stay aligned.'
+        components = @(
+            Get-AntDesignComponentDocumentation -Key 'button' -Title 'Button' -CommandName 'New-UDAntDesignButton' -SourceUrl 'https://ant.design/components/button/'
+            Get-AntDesignComponentDocumentation -Key 'typography' -Title 'Typography' -CommandName 'New-UDAntDesignTypography' -SourceUrl 'https://ant.design/components/typography'
+            Get-AntDesignComponentDocumentation -Key 'message' -Title 'Message' -CommandName 'Show-AntDesignMessage' -Category 'Feedback' -SourceUrl 'https://ant.design/components/message/'
+        )
+    }
+}
+
+function New-AntDesignDemoApp {
+    [CmdletBinding()]
+    param()
+
+    if (-not (Get-Command -Name 'New-UDApp' -ErrorAction Ignore)) {
+        throw 'New-AntDesignDemoApp requires PowerShell Universal and the Universal cmdlets to be loaded.'
+    }
+
+    New-UDApp -Title 'Ant Design Components' -Content {
+        New-AntDesignDemo
+    }
+}
