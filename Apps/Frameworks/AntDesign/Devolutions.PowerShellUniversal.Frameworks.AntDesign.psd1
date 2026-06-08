efd6723a00549f898d645b72692eebbc9e790945
Adam Driscoll <adamdriscoll@users.noreply.github.com>
2026-06-08 16:11:39 -0500

    Add rate control.

--- Apps/Frameworks/AntDesign/Devolutions.PowerShellUniversal.Frameworks.AntDesign.psd1
+++ Apps/Frameworks/AntDesign/Devolutions.PowerShellUniversal.Frameworks.AntDesign.psd1
@@ -11,6 +11,7 @@
         'Get-PSUAntDesignFrameworkEntryPoint',
         'New-UDAntDesignText',
         'New-UDAntDesignButton',
+        'New-UDAntDesignRate',
         'New-UDAntDesignSwitch',
         'New-UDAntDesignTypography',
         'Show-AntDesignMessage',
--- Apps/Frameworks/AntDesign/src/registry/registerBuiltins.tsx
+++ Apps/Frameworks/AntDesign/src/registry/registerBuiltins.tsx
@@ -1,6 +1,7 @@
 import { withComponentFeatures } from 'universal-dashboard';
 import { AntdButton } from '../components/AntdButton';
 import { AntdDocs } from '../components/AntdDocs';
+import { AntdRate } from '../components/AntdRate';
 import { AntdText } from '../components/AntdText';
 import { AntdSwitch } from '../components/AntdSwitch';
 import { AntdTypography } from '../components/AntdTypography';
@@ -19,6 +20,7 @@ export function registerBuiltins() {
 
   registerComponent('antd-button', wrapWithFeatures(AntdButton as RegisteredDashboardComponent));
   registerComponent('antd-docs', wrapWithFeatures(AntdDocs as RegisteredDashboardComponent));
+  registerComponent('antd-rate', wrapWithFeatures(AntdRate as RegisteredDashboardComponent));
   registerComponent('antd-switch', wrapWithFeatures(AntdSwitch as RegisteredDashboardComponent));
   registerComponent('antd-text', wrapWithFeatures(AntdText as RegisteredDashboardComponent));
   registerComponent('antd-typography', wrapWithFeatures(AntdTypography as RegisteredDashboardComponent));
